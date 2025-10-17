351ca7deb2da0eabb0f219509fa70e5dc8a852e7
Daria Starovoitova <d.starowoitova@yandex.ru>
2025-10-17 23:02:59 +0400

    Update main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int i = 120; // стоимость билета
+            int y = 7; // количество рублей для одной бонусной мили
+            int z = i/y; // количество бонусных миль
+            System.out.println(z); //вывод количества бонусных миль
+    }
+}
