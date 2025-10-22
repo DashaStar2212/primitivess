ace96b3ca818486eb9f66080ad737b2fb3272345
Daria Starovoitova <d.starowoitova@yandex.ru>
2025-10-22 20:35:23 +0400

    Update Main class

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            int ticketPrice = 120;
-            int rublesCount = 7;
-            int bonusMilesCount = ticketPrice/rublesCount;
-            System.out.println(bonusMilesCount);
+        int ticketPrice = 120;
+        int rublesCount = 7;
+        int bonusMilesCount = ticketPrice / rublesCount;
+        System.out.println(bonusMilesCount);
     }
 }
